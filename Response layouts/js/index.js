8fbfc03218f85c44cf561d41d47d0c025a99655e
kongchenglc@gmail.com <kongchenglc@gmail.com>
2017-07-12 18:57:58 +0800

    XUPTms

---
+++ Response layouts/js/index.js
@@ -0,0 +1,7 @@
+function menu_show() {
+    if(document.getElementById("menu-open").style.display=="none")
+    document.getElementById('menu-open').style.display="block";
+    else {
+        document.getElementById('menu-open').style.display="none";
+    }
+}
